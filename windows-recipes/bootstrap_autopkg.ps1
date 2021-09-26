731f242f12e9a4c966fca58c1b05a2f3b98e41de
Brandon Kurtz <kurtz.brandon@gmail.com>
2021-09-26 14:05:41 -0700

    adds script to automate python download

---
+++ windows-recipes/bootstrap_autopkg.ps1
@@ -0,0 +1,25 @@
+# $pyVersion is the Python version that will be downloaded
+$pyVersion = "3.9.5"
+# $pythonSavePath is place where the Python installer will be downloaded on disk
+$pythonSavePath = "~\Downloads\python-$pyVersion-amd64.exe"
+# $pythonInstallHash is the hash used to verify the Python installer download
+$pythonInstallHash = "53a354a15baed952ea9519a7f4d87c3f"
+# $pyEXEUrl is the url where the Python installer will be obtained
+$pyEXEUrl = "https://www.python.org/ftp/python/$pyVersion/python-$pyVersion-amd64.exe"
+
+# Download Python if needed
+if ((Test-Path $pythonSavePath) -eq $False) {
+    Write-Host "Downloading Python $pyVersion"
+    curl $pyEXEUrl -UseBasicParsing -OutFile $pythonSavePath
+} else {
+    Write-Host "Python $pyVersion was already downloaded"
+}
+
+# Verify hash of Python installer
+while ((Get-FileHash ($pythonSavePath) -Algorithm MD5).Hash -ne $pythonInstallHash) {
+    Write-Host "Python hashes did not match. Removing and redownloading"
+    rm $pythonSavePath
+    curl $pyEXEUrl -UseBasicParsing -OutFile $pythonSavePath
+}
+
+& $pythonSavePath Include_launcher=0 /passive
